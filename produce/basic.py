cb255ea873da26bae42d29a0566d9071c8fd4495
capricecorleone <2641725070@qq.com>
2024-04-14 00:22:29 +0800

    [feat]: 更新生产代码

--- produce/basic.py
+++ produce/basic.py
@@ -15,6 +15,9 @@ import pandas as pd
 
 from quant.core import Aligner, DataLoader, DataMaintainer, DataProcessor, AShareConsensus, FamaFrench3Factor
 
+aligner = Aligner()
+init_date = aligner.trade_dates.max()
+
 
 # %%
 if __name__ == "__main__":
@@ -22,9 +25,9 @@ if __name__ == "__main__":
     dm = DataMaintainer()
     dm.update_index()
     dm.update_stock_description()
-    dm.update_stock_quote(20050101)
-    dm.update_stock_size(20050101)
-    dm.update_index_quote(20050101)
+    dm.update_stock_quote(init_date)
+    dm.update_stock_size(init_date)
+    dm.update_index_quote(init_date)
 
     dp = DataProcessor([AShareConsensus, FamaFrench3Factor])
-    dp.run()
+    dp.run(init_date)
